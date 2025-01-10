7332acc87c56d1846d408ede25cbe94c82fa29c9
Jaan Laasalu <jaan.laasalu@krattworks.com>
2025-01-10 14:11:45 +0200

    initial commit

---
+++ mainprj/common/common.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include <list>
+#include <functional>
+#include <optional>
+
+using StringList = std::list<std::string>;
+
+namespace My::Common
+{
+	class IStreamData
+	{
+	public:
+		virtual const std::string& getStreamName() const = 0;
+		virtual void setStreamName(const std::string& name) = 0;
+
+		virtual const StringList& getStreamNames() const = 0;
+
+
+		virtual const std::optional<int> getFaceDir() const = 0;
+		virtual void setFaceDir(std::optional<int> f) = 0;
+
+		virtual const bool getSmiling() const = 0;
+		virtual void setSmiling(bool s) = 0;
+
+	};
+
+	class IStreamControl
+	{
+	public:
+		virtual bool run(bool start) = 0;
+		virtual bool isRunning() const = 0;
+	};
+
+	class IVideoDevice
+	{
+	public:
+		virtual bool start(const std::string& name) = 0;
+		virtual void stop() = 0;
+	};
+
+	using frameCallback = std::function<void(uint64_t, const uint8_t*, uint32_t, uint32_t)>;
+	class IVideoSource
+	{
+	public:
+		virtual ~IVideoSource() {}
+		virtual bool frame(frameCallback) = 0;
+		virtual float frameRate() = 0;
+	};
+
+
+}
---
+++ mainprj/main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include "Toolbox.h"
+#include "GuiContainer.h"
+#include "GuiVideoWindow.h"
+
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <chrono>
+#include <random>
+#include <limits>
+
+
+using namespace std::chrono_literals;
+
+class MyVideoSource : public My::Common::IVideoSource
+{
+	uint64_t sequenceNo{};
+	std::vector<uint32_t> buffer;
+	uint32_t width{ 1920 };
+	uint32_t height{ 1080 };
+
+	My::Toolbox::HitRate rate;
+
+	std::thread thrd;
+	std::mutex mtx;
+	std::condition_variable cond;
+	std::atomic_flag exit;
+
+	void threadFn()
+	{
+
+		std::mt19937 engine(std::random_device{}());
+		std::uniform_int_distribution<unsigned int> dist(static_cast<unsigned int>(0), static_cast<unsigned int>(0xffffffff));
+
+		std::unique_lock lck(mtx);
+		while (true)
+		{
+			cond.wait_for(lck, 333ms, [&exit = exit]() {return exit.test(); });
+			if (exit.test())
+			{
+				break;
+			}
+
+			auto num = dist(engine);
+			std::ranges::for_each(buffer, [&num](auto& pix) {pix = num; });
+			sequenceNo++;
+			rate.hit();
+
+		}
+	}
+
+
+public:
+
+	MyVideoSource()
+	{
+		buffer.resize(width * height);
+		thrd = std::thread(&MyVideoSource::threadFn, this);
+	}
+
+	~MyVideoSource() 
+	{
+		if (thrd.joinable())
+		{
+			exit.test_and_set();
+			cond.notify_one();
+			thrd.join();
+		}
+	}
+
+	bool frame(My::Common::frameCallback callback) override
+	{
+		std::lock_guard lck(mtx);
+		callback(sequenceNo, reinterpret_cast<uint8_t*>(buffer.data()), width, height);
+		return true;
+	}
+
+	virtual float frameRate() { return rate; };
+};
+
+
+MyVideoSource g_mvc;
+
+
+static int mine()
+{
+	My::Gui::GuiContainer container("Viewer", "1.0.0.0");
+	
+	My::Gui::VideoWindow videoW("Video", &g_mvc);
+	container.add("video", &videoW);
+
+	while (container.render()) {}
+	return 0;
+}
+
+int main()
+{
+	return mine();
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE /*hInstance*/,
+	_In_opt_ HINSTANCE /*hPrevInstance*/,
+	_In_ LPWSTR    /*lpCmdLine*/,
+	_In_ int       /*nCmdShow*/)
+{
+	return mine();
+
+}
+
+
